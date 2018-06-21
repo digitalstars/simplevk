172796496ff54a216751fc375a2c96708412d77f
digitalstarcompany <40269575+digitalstars@users.noreply.github.com>
2018-06-22 00:33:26 +0300

    Добавлена поддержка cURL в запросах к VK.

--- vk_api.php
+++ vk_api.php
@@ -82,13 +82,25 @@ class vk_api{
 		$url = 'https://api.vk.com/method/'.$method;
 		$params['access_token']=$this->token;
 		$params['v']='5.78';
-		$result = json_decode(file_get_contents($url, true, stream_context_create(array(
-		    'http' => array(
-		        'method'  => 'POST',
-		        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		        'content' => http_build_query($params)
-		    )
-		))), true);
+		if (function_exists('curl_init')) {
+			$ch = curl_init(); 
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+				"Content-Type:multipart/form-data"
+			));
+			curl_setopt($ch, CURLOPT_URL, $url); 
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $params); 
+			$result = json_decode(curl_exec($ch), True);
+			curl_close($ch);
+		} else {
+			$result = json_decode(file_get_contents($url, true, stream_context_create(array(
+			    'http' => array(
+			        'method'  => 'POST',
+			        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			        'content' => http_build_query($params)
+			    )
+			))), true);
+		}
 		if (isset($result['response']))
 			return $result['response'];
 		else
