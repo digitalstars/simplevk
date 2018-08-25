90c74e9fd566897f1652f2c1d66e38e3dc141583
ZeRoX <as234zx15@yandex.ru>
2018-08-25 22:49:30 +0300

    Добавлена авторизация через логин и пароль

--- test_posting.php
+++ test_posting.php
@@ -5,10 +5,11 @@ use DigitalStar\vk_api\vk_api as vk_api;
 use DigitalStar\vk_api\VkApiException as VkApiException;
 
 //**********CONFIG**************
-const VK_KEY = ""; //ключ авторизации через приложение
+const VK_KEY = "KEY"; //ключ авторизации через приложение
 const VERSION = "5.80"; //ваша версия используемого api
 //******************************
 try {
+    // $vk = new vk_api('login', 'pass', VERSION);
     $vk = new vk_api(VK_KEY, VERSION);
 
     $my_post = new Post($vk);
@@ -17,7 +18,7 @@ try {
 //    $my_post->addImage('img/goldfish.jpg', 'img/pink_salmon.jpg', 'img/plotva.jpg');
     $my_post->addImage('img/plotva.jpg');
     $my_post->addDocs('img/goldfish.jpg');
-    print_r( $my_post->send('-165686210', time() + 120) );
+    print_r( $my_post->send('381434758', time() + 120) );
 } catch (VkApiException $e) {
     print_r($e->getMessage());
 }
