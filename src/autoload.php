198dcb7905e701bd7ca6a02b97cb2978f4fa12b8
ZeRoX <as234zx15@yandex.ru>
2019-01-20 01:05:29 +0300

    Фикс autoload

--- src/autoload.php
+++ src/autoload.php
@@ -7,10 +7,10 @@
  */
 namespace DigitalStar\vk_api;
 
-require_once('Base.php');
-require_once('Message.php');
-require_once('Post.php');
-require_once('vk_api.php');
-require_once('VkApiException.php');
-require_once('auth.php');
-require_once('group.php');
+require_once(__DIR__.'/Base.php');
+require_once(__DIR__.'/Message.php');
+require_once(__DIR__.'/Post.php');
+require_once(__DIR__.'/vk_api.php');
+require_once(__DIR__.'/VkApiException.php');
+require_once(__DIR__.'/auth.php');
+require_once(__DIR__.'/group.php');
