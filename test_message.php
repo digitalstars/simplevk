dbab327917e5e35ab0faedc4d1cd3bcb613c886c
digitalstar-company <46852707+digitalstar-company@users.noreply.github.com>
2019-01-22 13:13:40 +0300

    Обновлен файл с примерами

--- test_message.php
+++ test_message.php
@@ -1,5 +1,5 @@
 <?php
-require_once('vk_api/autoload.php');
+require_once('vk_api/autoload.php'); //подключение новой библиотеки
 use DigitalStar\vk_api\Message as Message;
 use DigitalStar\vk_api\vk_api as vk_api;
 use DigitalStar\vk_api\VkApiException as VkApiException;
@@ -9,39 +9,49 @@ use DigitalStar\vk_api\Auth as Auth;
 //**********CONFIG**************
 const VK_KEY = "Key"; //ключ авторизации через приложение
 const VERSION = "5.80"; //ваша версия используемого api
-const VK_USERKEY = "User Key";
+const VK_USERKEY = "User_Key"; //ключ доступа пользователя
 //******************************
 
-$test_button = [["animals" => 'Fish'], "А какие бывают?", "blue"];
+$fish_button = [["animals" => 'Fish'], "А какие бывают?", "blue"]; //инициализация кнопки
 
 try {
-//    $vk = new vk_api('login', 'pass', VERSION);
-//    $vk = new vk_api(VK_USERKEY, VERSION);
-//    $vk = new vk_api($test, VERSION);
+    $vk_user = new vk_api('login', 'pass', VERSION); //авторизация пользователя через логин/пароль
+    //$vk_user = new vk_api(VK_USERKEY, VERSION); //авторизация через ключ пользователя
+    $vk_public = new vk_api(VK_KEY, VERSION); //авторизация через ключ группы
 
     /*------Отправка сообщения от имени пользователя---------*/
 
-    $my_msg = new Message($vk);
-    $my_msg->setMessage("Разных рыбин сообщение...");
-    $my_msg->addImage('img/goldfish.jpg');
-    $my_msg->addImage('img/pink_salmon.jpg');
-    $my_msg->addDocs('img/plotva.jpg');
-    print_r( $my_msg->send('105083531') );
+    $my_msg = new Message($vk_user); //объект конструктора сообщений
+    $my_msg->setMessage("Разных рыбин сообщение..."); //добавить в конструктор сообщение
+    $my_msg->addImage('img/goldfish.jpg'); //добавить в конструктор изображение
+    $my_msg->addImage('img/pink_salmon.jpg'); //еще одно изображение
+    $my_msg->addDocs('img/plotva.jpg'); //добавить изображение как документ
+    $my_msg->send('чей_то_id'); //отправить пользователю
+    $my_msg->send('чей_то_id'); //отправить другому полюзователю
 
-    /*---------От имени группы-------------------*/
+    /*---------Отправка сообщения от имени группы, используя аккаунт пользователя-------------------*/
 
-    $my_group = new group('148648911', $vk);
-
+    $my_group = new group('id_группы', $vk_user); //id группы где вы являетесь админом или создателем
     $my_msg = new Message($my_group);
     $my_msg->setMessage("Разных рыбин сообщение...");
-//    $my_msg->addImage('img/goldfish.jpg');
     $my_msg->addImage('img/pink_salmon.jpg');
     $my_msg->addDocs('img/plotva.jpg');
-    $my_msg->setKeyboard([]);
-    $my_msg->setKeyboard([[$test_button, $test_button],
-                            [$test_button, $test_button]], true);
-    print_r( $my_msg->send('105083531') );
+    $my_msg->setKeyboard([]); //если оставить такое, то у пользователя исчезнут кнопки, которые есть сейчас
+    $my_msg->setKeyboard([[$fish_button, $fish_button],
+                          [$fish_button, $fish_button]], true); //а это добавит к конструктору клавиатуру
+    $my_msg->send('чей_то_id');
+    
+    /*-----------------Простые функции отправки------------------*/
+    //все, кроме отправки клавиатуры работает и с объектом пользователя
+    $vk_public->sendMessage('чей_то_id', 'test'); //отправка сообщения
+    $vk_public->sendButton('чей_то_id', 'test', [[$fish_button, $fish_button]]); //отправка клавиатуры
+    $vk_public->sendImage('чей_то_id', 'img/pink_salmon.jpg'); //отправка клавиатуры
+    $vk_public->sendDocMessage('чей_то_id', 'img/pink_salmon.jpg', 'Свое_название картинки');
+    
 
 } catch (VkApiException $e) {
-    print_r($e->getMessage());
+    //если случится какая-то ошибка в библиотеке, то текст ошибки можно получить используя $e->getMessage()
+    print_r($e->getMessage()); 
+    //например можно отправить ошибку себе в вк
+    //$vk_user->sendMessage('ваш_id', $e->getMessage());
 }
