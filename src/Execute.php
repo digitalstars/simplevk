ee9f4be9d264c0fa6aa936a5becb582e05a8221a
ZeRoX <as234zx15@yandex.ru>
2019-04-26 21:28:45 +0300

    Добавлена поддержка execute для методов SendMessage, SendButtons и для конструктора сообщений

--- src/Execute.php
+++ src/Execute.php
@@ -16,9 +16,17 @@ class Execute extends vk_api {
         $this->vk = $vk;
     }
 
-    public function sendMessage($id, $message) {
+    public function sendMessage($id, $message, $props = []) {
+        $this->messages[] = ['peer_id' => $id, 'message' => $message, "random_id" => 0] + $props;
+        $this->counter += 1;
         $this->checkExec();
-        $this->messages[] = ['id' => $id, 'message' => $message];
+    }
+
+    public function sendButton($user_id, $message, $buttons = [], $one_time = False) {
+        $keyboard = $this->generateKeyboard($buttons, $one_time);
+        $this->messages[] = ['message' => $message, 'peer_id' => $user_id, 'keyboard' => $keyboard, "random_id" => 0];
+        $this->counter += 1;
+        $this->checkExec();
     }
 
     private function generateUrlPhotos($id, $count) {
@@ -28,26 +36,44 @@ class Execute extends vk_api {
         return $this->request("execute", ["code" => "return [".join(',', $code). "];"]);
     }
 
+    private function generateUrlDocs($id, $count) {
+        $code = [];
+        for ($i = 0; $i < $count; ++$i)
+            $code[] = "API.docs.getMessagesUploadServer({\"peer_id\" : $id, \"type\": \"doc\"})";
+        return $this->request("execute", ["code" => "return [".join(',', $code). "];"]);
+    }
+
     public function createMessages($id, $message = [], $props = [], $media = [], $keyboard = []) {
+
+        if (!isset($media['images']))
+            $media['images'] = [];
+        if (!isset($media['docs']))
+            $media['docs'] = [];
+
+        if (count($media['docs']) + count($media['images']) + 1 + $this->counter > Execute::$max_counter)
+            $this->exec();
 
         if (count($media['images']) != 0)
             $photo_urls = $this->generateUrlPhotos($id, count($media['images']));
+
+        if (count($media['docs']) != 0)
+            $doc_urls = $this->generateUrlDocs($id, count($media['docs']));
 
         if ($keyboard != [])
             $object = [
                 'id' => $id,
                 'message' => $message,
-                'keyboard' => true,
                 'keyboard_content' => $this->generateKeyboard($keyboard['keyboard'], $keyboard['one_time']),
-                'images_content' => []
+                'images_content' => [],
+                'docs_content' => []
             ];
         else
             $object = [
                 'id' => $id,
                 'message' => $message,
-                'keyboard' => false,
                 'keyboard_content' => [],
-                'images_content' => []
+                'images_content' => [],
+                'docs_content' => []
             ];
 
         foreach ($media as $selector => $massiv) {
@@ -70,14 +96,22 @@ class Execute extends vk_api {
                     }
                     break;
                 case "docs":
-//                    foreach ($massiv as $document) {
-//                        $upload_file = $this->uploadDocsMessages($id, $document['path'], $document['title']);
-//                        if (isset($upload_file['type']))
-//                            $upload_file = $upload_file[$upload_file['type']];
-//                        else
-//                            $upload_file = current($upload_file);
-//                        $send_attachment[] = "doc" . $upload_file['owner_id'] . "_" . $upload_file['id'];
-//                    }
+                    foreach ($massiv as $key => $document) {
+                        for ($i = 0; $i < $this->try_count_resend_file; ++$i) {
+                            try {
+                                $title = $document['title'] ?? preg_replace("!.*?/!", '', $document);
+                                $answer_vk = json_decode($this->sendFiles($doc_urls[$key]['upload_url'], $document['path']), true);
+                                $object['docs_content'][] = ['file' => $answer_vk['file'], 'title' => $title];
+                                $this->counter += 1;
+                                break;
+                            } catch (VkApiException $e) {
+                                sleep(1);
+                                $exception = json_decode($e->getMessage(), true);
+                                if ($exception['error']['error_code'] != 121)
+                                    throw new VkApiException($e->getMessage());
+                            }
+                        }
+                    }
                     break;
                 case "other":
                     break;
@@ -85,6 +119,7 @@ class Execute extends vk_api {
         }
         $this->counter += 1;
         $this->constructors_messages[] = $object;
+        $this->checkExec();
         return true;
     }
 
@@ -113,12 +148,14 @@ var count_image = 0;
 var text_attach_photo = "";
 var resulter = [];
 
+var data_result = [];
+
 while (query[count] != null) {
 	text_attach_photo = "";
 	resulter = [];
 	count_image = 0;
 	while (query[count]["images_content"][count_image] != null) {
-		resulter = API.photos.saveMessagesPhoto({"photo": query[count]["images_content"][count_image]["photo"], "server": query[count]["images_content"][count_image]["server"], "hash": query[count]["images_content"][count_image]["hash"]});
+		resulter = API.photos.saveMessagesPhoto(query[count]["images_content"][count_image]);
 		if (text_attach_photo == "") {
 			text_attach_photo = "photo" + resulter[0]["owner_id"] + "_" + resulter[0]["id"];
 		} else {
@@ -126,19 +163,31 @@ while (query[count] != null) {
 		}
 		count_image = count_image + 1;
 	}
-	API.messages.send({"peer_id": query[count]["id"], "message": query[count]["message"], "random_id": 0, "attachment": text_attach_photo, "keyboard": query[count]["keyboard_content"]});
+	count_image = 0;
+	while (query[count]["docs_content"][count_image] != null) {
+		resulter = API.docs.save(query[count]["docs_content"][count_image]);
+		if (text_attach_photo == "") {
+			text_attach_photo = "doc" + resulter["doc"]["owner_id"] + "_" + resulter["doc"]["id"];
+		} else {
+			text_attach_photo = text_attach_photo + ",doc" + resulter["doc"]["owner_id"] + "_" + resulter["doc"]["id"];
+		}
+		count_image = count_image + 1;
+	}
+	data_result.push(API.messages.send({"peer_id": query[count]["id"], "message": query[count]["message"], "random_id": 0, "attachment": text_attach_photo, "keyboard": query[count]["keyboard_content"]}));
 	count = count + 1;
 }
 
 count = 0;
 while (query_message[count] != null) {
-	API.messages.send({"peer_id": query_message[count]["id"], "message": query_message[count]["message"], "random_id": 0});
+	data_result.push(API.messages.send(query_message[count]));
 	count = count + 1;
 }
 
-return 1;';
+return data_result;';
         $this->messages = [];
         $this->constructors_messages = [];
-        return $this->request("execute", ["code" => $code]);
+        $res = $this->request("execute", ["code" => $code]);
+        print_r($res);
+        return $res;
     }
 }
