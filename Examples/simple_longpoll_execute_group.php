fbf0da2467a7f87574c070c493ca7d86767e6d82
ZeRoX <as234zx15@yandex.ru>
2019-04-27 17:55:20 +0300

    Дока + примеры

---
+++ Examples/simple_longpoll_execute_group.php
@@ -0,0 +1,18 @@
+<?php
+//require_once('../vendor/autoload.php'); //подключаем библу ЧЕРЕЗ COMPOSER
+require_once('../autoload.php'); //подключаем библу
+use DigitalStar\vk_api\vk_api;
+use DigitalStar\vk_api\LongPoll;
+use DigitalStar\vk_api\Execute;
+
+//**********CONFIG**************
+const VK_KEY = ""; //ключ авторизации сообщества, который вы получили
+//******************************
+$vk = vk_api::create(VK_KEY, '5.95');
+$vk = new Execute($vk);
+$vk = new LongPoll($vk);
+
+$vk->listen(function($data)use($vk){ //в $data содержится все данные события, можно убрать, если не нужен
+    $vk->initVars('id, message', $id, $message);
+    $vk->reply($message);
+});
