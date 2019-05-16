bb7f7684c758cf8696753006b6c2d1cd42e82f89
digitalstars <46852707+digitalstars@users.noreply.github.com>
2019-05-16 22:26:50 +0300

    Update simple_callback.php

--- Examples/simple_callback.php
+++ Examples/simple_callback.php
@@ -1,4 +1,8 @@
 <?php
+/** данные подключения правильные только в том случае,
+* если вы запускаете скрипт из папки Examples  из только что установленой либы. 
+* Иначе путь до autoload будет другой
+*/
 //require_once('../vendor/autoload.php'); //подключаем библу ЧЕРЕЗ COMPOSER
 require_once('../autoload.php'); //подключаем библу
 use DigitalStar\vk_api\vk_api;
@@ -11,5 +15,5 @@ const VERSION = "5.95"; //ваша версия используемого api
 
 $vk = vk_api::create(VK_KEY, VERSION)->setConfirm(CONFIRM_STR);
 $vk->debug();
-$vk->initVars('id, message', $id, $message); //инициализация переменных
+$vk->initVars($id, $message); //инициализация переменных
 $vk->reply('Секретная информация'); //отвечает пользователю или в беседу
