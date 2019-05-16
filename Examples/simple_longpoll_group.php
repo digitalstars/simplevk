833968c99c881f32df485fd07766e9ce2c81d765
digitalstars <46852707+digitalstars@users.noreply.github.com>
2019-05-16 22:28:26 +0300

    Update simple_longpoll_group.php

--- Examples/simple_longpoll_group.php
+++ Examples/simple_longpoll_group.php
@@ -1,4 +1,8 @@
 <?php
+/** данные подключения правильные только в том случае,
+* если вы запускаете скрипт из папки Examples  из только что установленой либы. 
+* Иначе путь до autoload будет другой
+*/
 //require_once('../vendor/autoload.php'); //подключаем библу ЧЕРЕЗ COMPOSER
 require_once('../autoload.php'); //подключаем библу
 use DigitalStar\vk_api\vk_api;
@@ -11,6 +15,6 @@ $vk = vk_api::create(VK_KEY, '5.95');
 $vk = new LongPoll($vk);
 
 $vk->listen(function($data)use($vk){ //в $data содержится все данные события, можно убрать, если не нужен
-    $vk->initVars('id, message', $id, $message);
+    $vk->initVars($id, $message);
     $vk->reply($message);
 });
