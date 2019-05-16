bf9ed49b078a2cc1336823f9a5d1a4a4ddcdc02c
digitalstars <46852707+digitalstars@users.noreply.github.com>
2019-05-16 22:22:00 +0300

    Update callback_beseda_keyboard.php

--- Examples/callback_beseda_keyboard.php
+++ Examples/callback_beseda_keyboard.php
@@ -1,4 +1,8 @@
 <?php
+/** данные подключения правильные только в том случае,
+* если вы запускаете скрипт из папки Examples  из только что установленой либы. 
+* Иначе путь до autoload будет другоц
+*/
 //require_once('../vendor/autoload.php'); //подключаем библу ЧЕРЕЗ COMPOSER
 require_once('../autoload.php'); //подключаем библу
 use DigitalStar\vk_api\vk_api;
@@ -12,10 +16,10 @@ const INFO =  [["command" => 'info'], "Информация", "blue"]; //Код 
 
 $vk = vk_api::create(VK_KEY, VERSION)->setConfirm(CONFIRM_STR);
 $vk->debug();
-$vk->initVars('id, message, payload', $id, $message, $payload); //инициализация переменных
+$vk->initVars($id, $message, $payload); //инициализация переменных
 
 if ($payload) { //если пришел payload
-    if($payload == 'info')
+    if($payload['command'] == 'info')
         $vk->reply('Секретная информация'); //отвечает пользователю или в беседу
 } else
     $vk->sendButton($id, 'Отправил кнопку', [[INFO]]); //отправляем клавиатуру с сообщением
