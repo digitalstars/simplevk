d24f658b895fbe24237c4ccfc355281abc4ceaa5
Runnin <mr.kirilll12@mail.ru>
2020-01-16 00:24:09 +0300

    3.0 alpha

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+namespace DigitalStars\SimpleVK;
+$path_dir =  __DIR__.'/src';
+//require_once($path_dir.'/Base.php');
+//require_once($path_dir.'/Message.php');
+//require_once($path_dir.'/Post.php');
+require_once($path_dir.'/SimpleVK.php');
+require_once($path_dir.'/SimpleVkException.php');
+require_once($path_dir.'/Auth.php');
+//require_once($path_dir.'/Group.php');
+require_once($path_dir.'/LongPoll.php');
+//require_once($path_dir.'/Coin.php');
+//require_once($path_dir.'/Execute.php');
+//require_once($path_dir.'/SiteAuth.php');
