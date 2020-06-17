cbfc2146482a54749ee426580c88d6ccc5e5bec9
zerox <as234zx12@yandex.ru>
2020-06-17 19:14:34 +0300

    Пример бота на командах

---
+++ examples/LongPool & BotConstructor1.php
@@ -0,0 +1,49 @@
+<?php
+require_once __DIR__ . '/../autoload.php';
+
+use DigitalStars\simplevk\Bot as Bot;
+use DigitalStars\SimpleVK\LongPoll as lp;
+
+$vk = new lp("", "5.110");
+$bot = Bot::create($vk);
+
+$bot->cmd('other')->text("Команда не найдена" .
+    "\nДоступные команды: " .
+    "\n!посчитай число + число \n---пример '!посчитай 5 + 5'" .
+    "\n!посчитай число * число \n---пример '!посчитай 5 * 5'" .
+    "\n!напиши мне слово любое_слово \n---пример '!напиши мне слово Привет!'" .
+    "\n!напиши что угодно \n---пример '!напиши Какой то рандомный текст'" .
+    "\n!покажи кнопку [слово] [white,blue,red,green] \n---пример '!покажи кнопку Кнопочка green'");
+
+$bot->cmd('sum', '!посчитай %n + %n')
+    ->func(function ($msg, $id, $params) {
+        $msg->text($params[0] + $params[1]);
+    });
+
+$bot->cmd('multiply', '!посчитай %n * %n')
+    ->func(function ($msg, $id, $params) {
+        $msg->text($params[0] * $params[1]);
+    });
+
+$bot->cmd('word', '!напиши мне слово %s')
+    ->func(function ($msg, $id, $params) {
+        $msg->text("Ваше слово: ".$params[0]);
+    });
+
+$bot->preg_cmd('more_word', "!\!напиши (.*)!")
+    ->func(function ($msg, $id, $params) {
+        $msg->text("Ваше предложение: ".$params[1]);
+    });
+
+$bot->cmd('send_btn', '!покажи кнопку %s %s')->text('Ваша кнопка: ')
+    ->func(function ($msg, $id, $params) use ($vk) {
+        if (!in_array($params[1], ['white', 'blue', 'red', 'green'])) {
+            $msg->text("Цвет ".$params[1]." не существует, использую 'white'\n".$msg->getText());
+            $params[1] = 'white';
+        }
+        $msg->kbd([[$vk->buttonText($params[0], $params[1])]]);
+    });
+
+$vk->listen(function ($data) use ($vk, $bot) {
+    $bot->run();
+});
