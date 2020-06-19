3cd9b8b1068012f4942062b95f93d295abe09844
zerox <as234zx12@yandex.ru>
2020-06-19 19:06:46 +0300

    Задача дефолтного цвета, множественные маски, компиляция и загрузка

--- src/Bot.php
+++ src/Bot.php
@@ -9,6 +9,7 @@ class Bot {
     private $config = [];
     private $is_text_start = false;
     private $status = 1;
+    private $color = 'white';
 
     public function __construct($vk) {
         $this->vk = $vk;
@@ -41,13 +42,13 @@ class Bot {
         if (isset($btn)) {
             if (is_array($btn)) {
                 if (!isset($btn[1]))
-                    $btn[1] = 'white';
+                    $btn[1] = $this->color;
                 if (count($btn) == 2 and in_array($btn[1], ['white', 'green', 'red', 'blue']))
                     $this->config['btn'][$id] = $this->vk->buttonText($btn[0], $btn[1]);
                 else
                     $this->config['btn'][$id] = $btn;
             } else
-                $this->config['btn'][$id] = $this->vk->buttonText($btn);
+                $this->config['btn'][$id] = $this->vk->buttonText($btn, $this->color);
         }
         if ($is_text_triggered)
             $this->cmd($id, $btn[2]);
@@ -55,18 +56,21 @@ class Bot {
     }
 
     public function __call($name, $arguments) {
-        if (empty($arguments))
-            return $this->btn($name);
-        if (is_array($arguments[0]))
-            return $this->btn($name, $arguments[0]);
-        return $this->btn($name, $this->vk->buttonText($arguments[0], $arguments[1] ?? 'white'));
+        return $this->btn($name, $arguments[0] ?? null, $arguments[1] ?? null);
     }
 
     public function cmd($id, $mask = null, $is_case = false) {
-        if (!$is_case)
-            $mask = mb_strtolower($mask);
-        if (isset($mask))
-            $this->config['mask'][$id] = [$mask, $is_case];
+        if (isset($mask)) {
+            $this->config['mask'][$id] = [[], $is_case];
+            if (is_array($mask))
+                foreach ($mask as $m) {
+                    if (!$is_case)
+                        $m = mb_strtolower($m);
+                    $this->config['mask'][$id][0][] = $m;
+                }
+            else
+                $this->config['mask'][$id][0][] = $mask;
+        }
         return $this->newAction($id);
     }
 
@@ -87,9 +91,77 @@ class Bot {
 
     private function runAction($id, &$action, $result_parse = null) {
         $this->status = 0;
-        $result = Message::create($this->vk, $action, $this, $this->config['btn'])->send($id, null, $result_parse);
-        $this->status = 1;
-        return $result;
+        return Message::create($this->vk, $action, $this, $this->config['btn'])->send($id, null, $result_parse);
+    }
+
+    private function out_array($array, $var, $_livel = null) {
+        $out = $margin = '';
+        $nr = "\n";
+        $tab = "\t";
+
+        if (is_null($_livel)) {
+            $out .= '<?php' . PHP_EOL . '$' . $var . ' = ';
+            if (!empty($array)) {
+                $out .= $this->out_array($array, $var, 0);
+            }
+            $out .= ';';
+        } else {
+            for ($n = 1; $n <= $_livel; $n++) {
+                $margin .= $tab;
+            }
+            $_livel++;
+            if (is_array($array)) {
+                $i = 1;
+                $count = count($array);
+                $out .= '[' . $nr;
+                foreach ($array as $key => $row) {
+                    $out .= $margin . $tab;
+                    if (is_numeric($key)) {
+                        $out .= $key . ' => ';
+                    } else {
+                        $out .= "'" . $key . "' => ";
+                    }
+
+                    if (is_array($row)) {
+                        $out .= $this->out_array($row, $var, $_livel);
+                    } elseif (is_null($row)) {
+                        $out .= 'null';
+                    } elseif (is_numeric($row)) {
+                        $out .= $row;
+                    } elseif (is_bool($row)) {
+                        $out .= ($row) ? 'true' : 'false';
+                    } else {
+                        $out .= "'" . addslashes($row) . "'";
+                    }
+                    if ($count > $i) {
+                        $out .= ',';
+                    }
+                    $out .= $nr;
+                    $i++;
+                }
+                $out .= $margin . ']';
+            } else {
+                $out .= "'" . addslashes($array) . "'";
+            }
+        }
+        return $out;
+    }
+
+    public function compile($var = 'compile') {
+        return $this->out_array($this->config, $var);
+    }
+
+    public function load($compile) {
+        $this->config = $compile;
+        return $this;
+    }
+
+    public function setDefaultColor($color) {
+        if (in_array($color, ['white', 'green', 'red', 'blue']))
+            $this->color = $color;
+        else
+            throw new SimpleVkException(0, "Неверное название цвета");
+        return $this;
     }
 
     public function run($send = null, $id = null) {
@@ -106,27 +178,28 @@ class Bot {
         if (!empty($message)) {
             if (isset($this->config['mask'])) {
                 $arr_msg = explode(' ', $message);
-                foreach ($this->config['mask'] as $action => $mask) {
-                    $mask_words = explode(' ', $mask[0]);
-                    if (count($mask_words) != count($arr_msg))
-                        continue;
-                    $flag = true;
-                    $result_parse = [];
-                    foreach ($mask_words as $index => $word) {
-                        if ($word == '%n') {
-                            $number_temp = str_replace(",", '.', $arr_msg[$index]);
-                            if (is_numeric($number_temp))
-                                $result_parse[] = (double)$number_temp;
-                        } else if ($word == '%s' and is_string($arr_msg[$index])) {
-                            $result_parse[] = $arr_msg[$index];
-                        } else if ((!$mask[1] or $word != $arr_msg[$index]) and ($mask[1] or $word != mb_strtolower($arr_msg[$index]))) {
-                            $flag = false;
-                            break;
+                foreach ($this->config['mask'] as $action => $masks)
+                    foreach ($masks[0] as $mask){
+                        $mask_words = explode(' ', $mask);
+                        if (count($mask_words) != count($arr_msg))
+                            continue;
+                        $flag = true;
+                        $result_parse = [];
+                        foreach ($mask_words as $index => $word) {
+                            if ($word == '%n') {
+                                $number_temp = str_replace(",", '.', $arr_msg[$index]);
+                                if (is_numeric($number_temp))
+                                    $result_parse[] = (double)$number_temp;
+                            } else if ($word == '%s' and is_string($arr_msg[$index])) {
+                                $result_parse[] = $arr_msg[$index];
+                            } else if ((!$masks[1] or $word != $arr_msg[$index]) and ($masks[1] or $word != mb_strtolower($arr_msg[$index]))) {
+                                $flag = false;
+                                break;
+                            }
                         }
+                        if ($flag)
+                            return $this->runAction($id, $this->config['action'][$action], $result_parse);
                     }
-                    if ($flag)
-                        return $this->runAction($id, $this->config['action'][$action], $result_parse);
-                }
             }
             if (isset($this->config['preg_mask']))
                 foreach ($this->config['preg_mask'] as $action => $preg_mask)
