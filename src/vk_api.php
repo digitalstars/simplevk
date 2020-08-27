5bf04712c6ce34cfa21d6fe7ad93212597cd0a1b
DigitalStars <46852707+digitalstars@users.noreply.github.com>
2020-08-27 20:14:54 +0300

    Фикс отправки файлов на windows

--- src/vk_api.php
+++ src/vk_api.php
@@ -631,6 +631,8 @@ class vk_api {
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             $output = curl_exec($ch);
             if ($output != '')
                 break;
